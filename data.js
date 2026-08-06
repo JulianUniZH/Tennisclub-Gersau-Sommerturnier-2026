3a31cc713309645fe15aab8d7ecf944a5b5ad00b
JulianUniZH <163400443+JulianUniZH@users.noreply.github.com>
2026-08-06 10:53:39 +0200

    Update data.js
    Marco - Gianfranco

--- data.js
+++ data.js
@@ -6,7 +6,7 @@ const tournament = {
   title: "Tennisclub Gersau Sommerturnier 2026",
   subtitle: "Gruppenphase · 4 Gruppen à 5 Spieler & 2 Tableaus",
   pointsForWin: 1,
-  updatedAt: "2026-08-04 17:50",
+  updatedAt: "2026-08-06 10:50",
   groups: {
     "Gruppe A": [
       "Mitch Steffan",
@@ -79,7 +79,7 @@ const tournament = {
     { group: "Gruppe D", p1: "Enzo Nigg", p2: "Gianfranco Da Palma", score: "6:4 7:5" },
     { group: "Gruppe D", p1: "Marco Camenzind", p2: "Urs Huber", score: "0:6 1:6" },
     { group: "Gruppe D", p1: "Vasco Sommacal", p2: "Enzo Nigg", score: "6:2 6:1" },
-    { group: "Gruppe D", p1: "Gianfranco Da Palma", p2: "Marco Camenzind", score: "" },
+    { group: "Gruppe D", p1: "Gianfranco Da Palma", p2: "Marco Camenzind", score: "2:6 1:6" },
     { group: "Gruppe D", p1: "Urs Huber", p2: "Enzo Nigg", score: "6:0 6:0" },
     { group: "Gruppe D", p1: "Vasco Sommacal", p2: "Gianfranco Da Palma", score: "6:2 6:2" },
     { group: "Gruppe D", p1: "Enzo Nigg", p2: "Marco Camenzind", score: "6:4 6:4" },
