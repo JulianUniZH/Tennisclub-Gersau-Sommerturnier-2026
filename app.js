43cf63f865a85ae677432a130af4679215be3665
JulianUniZH <163400443+JulianUniZH@users.noreply.github.com>
2026-08-14 22:24:48 +0200

    Update app.js
    Lars W.O. in Tableau 2

--- app.js
+++ app.js
@@ -222,11 +222,11 @@ function placementBracketSvg() {
 
   const m = [
     { x: x1, y: 0,   a: "Vierter Gruppe A", b: "Gianfranco Da Palma (5. D)", score: "" },
-    { x: x1, y: 170, a: "Vierter Gruppe B", b: "Lars Nigg (5. C)", score: "" },
+    { x: x1, y: 170, a: "Vierter Gruppe B", b: "Lars Nigg (5. C)", score: "w.o." },
     { x: x1, y: 360, a: "Petra Rotter (4. C)", b: "Alan Tarantino (5. B)", score: "" },
     { x: x1, y: 530, a: "Enzo Nigg (4. D)", b: "Mitch Steffan (5. A)", score: "w.o." },
 
-    { x: x2, y: 85,  a: "", b: "", score: "" },
+    { x: x2, y: 85,  a: "", b: "Vierter Gruppe B", score: "" },
     { x: x2, y: 445, a: "", b: "Enzo Nigg", score: "" },
 
     { x: x3, y: 265, a: "", b: "", score: "" }
