733034f87a07574bd9a19c29d334caae7e975709
JulianUniZH <163400443+JulianUniZH@users.noreply.github.com>
2026-09-06 13:41:53 +0200

    Update app.js
    Chrischny - Öle

--- app.js
+++ app.js
@@ -167,14 +167,14 @@ function mainBracketSvg() {
 
     { x: x1, y: 420, a: "-",                b: "-", score: "" },
     { x: x1, y: 510, a: "Christian Schnyder (2. A)", b: "Marco Appert (3. B)", score: "6:0 6:0" },
-    { x: x2, y: 465, a: "Oliver Bolfing (1. C)",  b: "Christian Schnyder", score: "" },
+    { x: x2, y: 465, a: "Oliver Bolfing (1. C)",  b: "Christian Schnyder", score: "2:6 3:6" },
 
     { x: x1, y: 630, a: "-",                b: "-", score: "" },
     { x: x1, y: 720, a: "Pascal Steiner (2. B)", b: "Sabine Allemann (3. A)", score: "6:0 6:2" },
     { x: x2, y: 675, a: "Urs Huber (1. D)",  b: "Pascal Steiner", score: "" },
 
     { x: x3, y: 150, a: "", b: "", score: "" },
-    { x: x3, y: 570, a: "", b: "", score: "" },
+    { x: x3, y: 570, a: "Christian Schnyder", b: "", score: "" },
 
     { x: x4, y: 360, a: "", b: "", score: "" }
   ];
