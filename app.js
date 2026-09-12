3ffb0f74f393bad591fa0cd7491b795c215ea424
JulianUniZH <163400443+JulianUniZH@users.noreply.github.com>
2026-09-12 17:33:02 +0200

    Update app.js
    Jüle - Marco

--- app.js
+++ app.js
@@ -163,7 +163,7 @@ function mainBracketSvg() {
 
     { x: x1, y: 210, a: "-",                b: "-", score: "" },
     { x: x1, y: 300, a: "Marco Camenzind (2. D)", b: "Philip Camenzind (3. C)", score: "6:3 6:4" },
-    { x: x2, y: 255, a: "Julian Bolfing (1. B)",  b: "Marco Camenzind", score: "" },
+    { x: x2, y: 255, a: "Julian Bolfing (1. B)",  b: "Marco Camenzind", score: "6:1 6:2" },
 
     { x: x1, y: 420, a: "-",                b: "-", score: "" },
     { x: x1, y: 510, a: "Christian Schnyder (2. A)", b: "Marco Appert (3. B)", score: "6:0 6:0" },
